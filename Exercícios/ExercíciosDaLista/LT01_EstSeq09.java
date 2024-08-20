cab8ccbe15946a1fb16aa655e35c657793b250bf
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-08-19 22:00:12 -0300

    LT01_EstSeq09
    Soma dos quadrados

---
+++ Exercícios/ExercíciosDaLista/LT01_EstSeq09.java
@@ -0,0 +1,15 @@
+/**
+ * LT01_EstSeq09
+ */
+import javax.swing.JOptionPane;
+public class LT01_EstSeq09 {
+    public static void main(String args[]){
+        int x, y, a, b, c;
+        x = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de X"));
+        y = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de Y"));
+        a = x * x;
+        b = y * y;
+        c = a + b;
+        JOptionPane.showMessageDialog(null,"A soma dos quadrados é igual a"+ c);
+    }
+}
