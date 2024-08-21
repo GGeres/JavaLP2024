9c3501e0d90c9bae1070e7d369a686e46ed16840
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-08-20 23:31:19 -0300

    LT01_EstSeq14
    Calculo do 3º angulo de um triângulo

--- Exercícios/ExercíciosDaLista/LT01_EstSeq14.java
+++ Exercícios/ExercíciosDaLista/LT01_EstSeq14.java
@@ -1,3 +1,13 @@
+/**
+ * LT01_EstSeq14
+ */
+import javax.swing.JOptionPane;
 public class LT01_EstSeq14 {
-    
+    public static void main(String[] args) {
+        int a, b, c;
+        a = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do primeiro ângulo"));
+        b = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do segundo ângulo"));
+        c = (180 - (a + b));
+        JOptionPane.showMessageDialog(null, "O valor do terceiro ângulo é de "+c+"°.");
+    }
 }
