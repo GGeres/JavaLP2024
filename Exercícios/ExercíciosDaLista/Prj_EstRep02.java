2f61d884cac3894d063c8e88d256e5617be1c61c
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-08-28 21:39:53 -0300

    Prj_EstRep02

---
+++ Exercícios/ExercíciosDaLista/Prj_EstRep02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * Gustavo Geres
+ * Calcule e mostre o quadrado dos números entre 10 e 150
+ */
+import javax.swing.JOptionPane;
+public class Prj_EstRep02 {
+    public static void main(String[] args) {
+        int num, quad;
+        num = 10;
+        while(num >= 10 && num <= 150){
+            quad = num * num;
+            System.out.println("O quadrado de "+num+" é "+quad);
+            num++;
+        }
+    }
+}
