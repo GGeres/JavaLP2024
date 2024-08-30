dc05fd43206e5fd48492eee2a0962a42d99e0502
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-08-29 22:05:47 -0300

    Update

--- Exercícios/ExercíciosDaLista/Modularizações/Procedimento/LT01_EstMod21.java
+++ Exercícios/ExercíciosDaLista/Modularizações/Procedimento/LT01_EstMod21.java
@@ -3,33 +3,15 @@
  * Gustavo Jorge Geres
  * 
  * LT01_EstMod21
- * Receba 4 notas bimestrais de um aluno. Calcule e mostre a média aritmética. Mostre a mensagem de acordo com a média: - Feito
-    a.	Se a média for >= 6,0 exibir “APROVADO”;
-    b.	Se a média for >= 3,0 ou < 6,0 exibir “EXAME”;
-    c.	Se a média for < 3,0 exibir “RETIDO”.
-
+ * Receba 3 coeficientes A, B, e C de uma equação do 2º grau 
+ * da fórmula AX²+BX+C=0. Verifique e mostre a existência 
+ * de raízes reais e se caso exista, calcule e mostre. 
  * 29/08/2024
  * 
  */
 import javax.swing.JOptionPane;
 public class LT01_EstMod21 {
-    static long n1, n2, n3, n4, mBim;
     public static void main(String[] args) {
-        n1 = Long.parseLong(JOptionPane.showInputDialog("Digite a primeira nota"));
-        n2 = Long.parseLong(JOptionPane.showInputDialog("Digite a segunda nota"));
-        n3 = Long.parseLong(JOptionPane.showInputDialog("Digite a terceira nota"));
-        n4 = Long.parseLong(JOptionPane.showInputDialog("Digite a quarta nota"));
-        CalcMediaBim();
-    }
-
-    static void CalcMediaBim(){
-        mBim = ((n1 + n2 + n3 + n4) / 4);
-        if (mBim >= 6) {
-            JOptionPane.showMessageDialog(null,"Aprovado! Sua nota: "+mBim);
-        } else if (mBim >= 3) {
-            JOptionPane.showMessageDialog(null,"Exame. Sua nota: "+mBim);
-        } else{
-            JOptionPane.showMessageDialog(null,"Retido. Sua nota: "+mBim);
-        }
+        
     }
 }
