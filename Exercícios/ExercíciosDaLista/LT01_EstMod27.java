896192e8f9dae97f56acf68165a560e5a0d9d6d5
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-09-12 10:56:43 -0300

    Update

---
+++ Exercícios/ExercíciosDaLista/LT01_EstMod27.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Gustavo Jorge Geres
+ * 
+ * LT01_EstMod27
+ * Receba o número de voltas, a extensão do circuito (em metros) e o 
+ * tempo de duração (minutos). Calcule e mostre a velocidade média em km/h.    
+ * 12/09/2024
+ * 
+ **/
+import javax.swing.JOptionPane;
+public class LT01_EstMod27 {
+    public static void main(String[] args) {
+        
+    }
+}
